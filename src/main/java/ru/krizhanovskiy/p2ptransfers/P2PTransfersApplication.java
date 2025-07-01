77853ae9fdcae47324134bcefa0143832a2af64c
Krizhanovskiy <mrdoka1@mail.ru>
2025-07-01 21:38:27 +0300

    init and start setting

---
+++ src/main/java/ru/krizhanovskiy/p2ptransfers/P2PTransfersApplication.java
@@ -0,0 +1,13 @@
+package ru.krizhanovskiy.p2ptransfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PTransfersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2PTransfersApplication.class, args);
+    }
+
+}
